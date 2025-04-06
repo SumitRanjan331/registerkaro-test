792387806e73c4d9057ddb2251c67810f9aa179c
Sumit <sumitranjan331@gmail.com>
2025-04-06 19:38:52 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Features from "./components/Features";
+import SubmitForm from "./components/SubmitForm";
+import Footer from "./components/Footer";
+import TopBar from "./components/TopBar";
+import Services from "./components/Services";
+import AboutSection from "./components/AboutSection";
+import ChooseUs from "./components/ChooseUs";
+import VideoIntroduction from "./components/VideoIntroduction";
+import HappyClients from "./components/HappyClients";
+import BlogGrid from "./components/BlogGrid";
+import Testimonials from "./components/Testimonials";
+import Faq from "./components/Faq";
+import Promotions from "./components/Promotions";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopBar/>
+      <Header />
+      <HeroSection />
+      <Features />
+      <Services/>
+      <AboutSection/>
+      <ChooseUs/>
+      <VideoIntroduction/>
+      <HappyClients/>
+      <BlogGrid/>
+      <Testimonials/>
+      <Faq/>
+      <Promotions/>
+      <SubmitForm/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
